951e07f6bab37079c4218fe281fbebaa7904e1b1
Aishwarya <aishwarya.saibewar@peopletech.com>
2024-10-14 15:14:52 -0700

    updated code

--- frontend/testcase.js
+++ frontend/testcase.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import Login from './Login';
+import Login from './Login.js';
 
 describe('Login Component', () => {
   test('renders without crashing', () => {
