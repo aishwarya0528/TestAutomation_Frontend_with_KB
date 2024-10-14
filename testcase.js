dd4032796f3c4ad9199c708e5eca9b00b5c92c82
Aishwarya - PTG <aishwarya.saibewar@peopletech.com>
2024-10-14 15:19:41 -0700

    Update testcase.js

--- testcase.js
+++ testcase.js
@@ -1,6 +1,3 @@
-Here's the Jest test code for the Login component:
-
-```javascript
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import Login from './Login';
@@ -97,4 +94,3 @@ describe('Login Component', () => {
     expect(loginButton).toHaveFocus();
   });
 });
-```
