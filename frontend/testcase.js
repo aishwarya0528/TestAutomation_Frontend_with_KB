65d89ceec9ca3641cd8b9fd7b67ad469aa6747e0
Aishwarya - PTG <aishwarya.saibewar@peopletech.com>
2024-10-16 17:14:14 -0700

    Initial test automation code

--- frontend/testcase.js
+++ frontend/testcase.js
@@ -1,3 +1,6 @@
+Here's the Jest test code for the Login component:
+
+```javascript
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -81,3 +84,4 @@ describe('Login Component', () => {
     expect(errorMessage).toBeInTheDocument();
   });
 });
+```
