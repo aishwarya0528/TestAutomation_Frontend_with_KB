c4f2adbf2aeac29bebdea491a53c35de68330677
Aishwarya S <aishwarya.saibewar@peopletech.com>
2024-11-12 11:27:31 -0800

    Update testcase.js

--- frontend/testcase.js
+++ frontend/testcase.js
@@ -1,6 +1,3 @@
-Here's the Jest test code for the Login component:
-
-```javascript
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -85,4 +82,3 @@ describe('Login Component', () => {
     consoleSpy.mockRestore();
   });
 });
-```
