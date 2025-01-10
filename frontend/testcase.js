3f71f743a9aae45c345ab1f91eaacde4a7503baa
Tanuj-Koduri <tanuj.koduri@peopletech.com>
2025-01-10 15:17:58 -0800

    Update testcase.js

--- frontend/testcase.js
+++ frontend/testcase.js
@@ -1,6 +1,3 @@
-Here's the Jest test code for the Login component:
-
-```javascript
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -60,4 +57,3 @@ describe('Login Component', () => {
     expect(screen.getByLabelText('Password:')).toHaveValue('');
   });
 });
-```
